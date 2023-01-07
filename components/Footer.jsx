6f0ebc3550e74b41f6cac5ee5a65b7240a550df3
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2023-01-07 23:30:59 +0530

    Front-end of Dapp

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
         <div className="flexStart flex-1 flex-col">
           <div className="flexCenter cursor-pointer">
             <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="logo" />
-            <p className=" dark:text-white text-nft-dark font-semibold text-lg ml-1">CryptoKet</p>
+            <p className=" dark:text-white text-nft-dark font-semibold text-lg ml-1">Artium</p>
           </div>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base mt-6">Get the latest updates</p>
           <div className="flexBetween md:w-full minlg:w-557 w-357 mt-6 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 rounded-md">
@@ -36,7 +36,7 @@ const Footer = () => {
         </div>
 
         <div className="flex-1 flexBetweenStart flex-wrap ml-10 md:ml-0 md:mt-8">
-          <FooterLinks heading="CryptoKet" items={['Explore', 'How it Works', 'Contact Us']} />
+          <FooterLinks heading="Artium" items={['Explore', 'How it Works', 'Contact Us']} />
           <FooterLinks heading="Support" items={['Help Center', 'Terms of service', 'Legal', 'Privacy policy']} extraClasses="ml-4" />
         </div>
       </div>
